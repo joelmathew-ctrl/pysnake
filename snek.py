6e68196d5fdef7e5a081863ec3f3beeefcef92b9
Joel Mathew <joel.mathew.workemail@gmail.com>
2025-02-09 14:35:37 +0400

    Rendered map borders and snake

--- snek.py
+++ snek.py
@@ -3,6 +3,12 @@
 
 import numpy as np 
 
+# Defining directions 
+UP = (0,1)
+DOWN = (0,-1)
+LEFT = (-1,0)
+RIGHT = (1,0)
+
 class Snake:
     def __init__(self, init_body, init_direction):
         self.body = init_body
@@ -24,13 +30,34 @@ class Game:
     def __init__(self, height, width):
         self.height = height
         self.width = width
+        self.snake = Snake([(1,1), (2,1), (3,1), (4,1)], UP)
 
     def board_matrix(self):
         # Return a matrix filled with spaces
-        return np.full((self.height, self.width), " ")
+        board = np.full((self.height, self.width), " ")
+        for y in range(self.height):
+            for x in range(self.width):
+                board[0][x] = "-"
+                board[y][0] = "|"
+                board[self.height-1][x] = "-"
+                board[y][self.width-1] = "|"
+
+        board[0][0] = "+"
+        board[self.height-1][self.width-1] = "+"
+        board[self.height-1][0] = "+"
+        board[0][self.width-1] = "+"
+
+        return board
 
     def render(self):
         board = self.board_matrix()
+        for x in range(len(self.snake.body)):
+            x_coord, y_coord = self.snake.body[x]
+            board[x_coord][y_coord] = "o"
+        
+        x_coord_head, y_coord_head = self.snake.head()
+        board[x_coord_head][y_coord_head] = "x"
+        
         for y in range(self.height):
             print("".join(board[y]))  # Join elements of each row into a single string
 
